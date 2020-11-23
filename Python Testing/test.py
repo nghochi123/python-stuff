a1402d129d8886e55d664354529bd6f56fb1a082
nghochi123 <35862661+nghochi123@users.noreply.github.com>
2020-11-23 12:11:52 +0800

    python stuff

---
+++ Python Testing/test.py
@@ -0,0 +1,10 @@
+import unittest
+import cap
+
+class TestCap(unittest.TestCase):
+    def test_one_word(self):
+        text = 'python'
+        result = cap.cap_text(text)
+        self.assertEqual(result, 'Sython')
+
+unittest.main()
